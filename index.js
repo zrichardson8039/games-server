935f77801b6e3b91ff279ba91747e7bbd12fcde5
zrichardson8039 <zachrichardson1114@gmail.com>
2021-12-18 17:35:20 -0500

    aded firebase url to cors origin list

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5005
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://zachs-ai-games.web.app", "http://localhost:3000"],
     allowedHeaders: ["my-custom-header"],
     credentials: false,
   },
