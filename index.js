e08ce096c2629d57c3700782cf9d365996652ec3
zrichardson8039 <zachrichardson1114@gmail.com>
2021-12-18 17:43:37 -0500

    fixed CORS

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const port = process.env.PORT || 5005
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "https://zachs-ai-games.web.app/tic-tac-toe",
-      "http://localhost:3000",
-    ],
+    origin: ["https://zachs-ai-games.web.app", "http://localhost:3000"],
     allowedHeaders: ["my-custom-header"],
     credentials: false,
   },
